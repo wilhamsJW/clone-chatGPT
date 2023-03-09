14de38e5a028b1d288792f0bfbe9936773e54c7e
wilhams junior <wilhams.junior@mundiale.com.br>
2023-03-09 20:51:47 -0300

    build: construction the routing file

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const cors = require("cors")
+
+const routes = require("./route/routes")
+
+require("dotenv").config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use(routes)
+
+module.exports = app
